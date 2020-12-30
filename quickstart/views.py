903c7d4875aa0d5140e90da8973620817084dfa5
conoro24 <conorbrian99@gmail.com>
2020-12-30 14:21:58 -0500

    attempts to import from github but not displaying

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from github import Github
 
 # Create your views here.
+username = 'conoro24'
+g = Github()
 
+user = g.get_user(username)
+for repo in user.get_repos():
+    commits = repo.get_commits().totalCount
+    name = repo.full_name
 
 def indexPage(request):
+	varA = name
+	varB = commits
 	context = {'a':'a'}
 	return render(request,'index.html', context)
